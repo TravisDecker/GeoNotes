200952da1002317c8205c37c53764ff659e6742e
Travis Decker <decker4693@gmail.com>
2019-01-01 21:55:04 -0700

    Initial Commit

---
+++ app/src/main/java/com/straylense/geonotes/GridFragment.java
@@ -0,0 +1,45 @@
+package com.straylense.geonotes;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class GridFragment extends Fragment {
+
+    static GridFragment fragment;
+
+    public static GridFragment newInstance() {
+
+        if (fragment == null) {
+            fragment = new GridFragment();
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.grid_fragment, container, false);
+
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getContext()));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/straylense/geonotes/ImageAdapter.java
@@ -0,0 +1,35 @@
+package com.straylense.geonotes;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+
+    public ImageAdapter(Context c) {
+        context = c;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
